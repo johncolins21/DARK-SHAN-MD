66480e2240543cb754727c849405dac20bdcf727
johncolins21 <85611520+johncolins21@users.noreply.github.com>
2024-05-13 13:12:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ global.gurl = 'https://instagram.com/naveeddogar_' // add your username
 global.sudo = process.env.SUDO || '9471234567'
 global.devs = '94772108460';
 global.website = 'https://github.com/kushansewmina1234/DARK-SHAN-MD' // Hello world
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ee0f303f5da6d86d024e0.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/WW3qVz8.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'DARK-SHAN-MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'DARKSHAN' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? 'John Ai Bot' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'johncolins' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
   author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? 'DARKSHAN' : process.env.PACK_AUTHER.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS  || 'true' ,
